e023469dbf43f6866fee3bdcbebec416dbd9f037
YOUR-USERNAME <gtellez07@yahoo.com>
2023-02-13 16:35:13 -0600

    added schema inside admin.py to display on admin page

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Show
+
+
+@admin.register(Show)
+class ShowAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'genre', 'description', 'review')
