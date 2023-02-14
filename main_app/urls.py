745fbfac5a16f845d24cb82f6365d035e3b8e207
YOUR-USERNAME <gtellez07@yahoo.com>
2023-02-13 20:51:42 -0600

    created view to handle adding, updating and deleting tv shows

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+from .views import ShowCreateView, ShowUpdateView, ShowDeleteView
 
 router = routers.DefaultRouter()
 
@@ -10,4 +11,8 @@ urlpatterns = [
     path('api/shows/', views.ShowList.as_view()),
     path('api/shows/<int:tv_id>/', views.show_details),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('shows/', views.ShowListView.as_view(), name='show_list'),
+    path('show/create/', ShowCreateView.as_view(), name='show_create'),
+    path('show/<int:tv_pk>/update/', ShowUpdateView.as_view(), name='show_update'),
+    path('show/<int:tv_pk>/delete/', ShowDeleteView.as_view(), name='show_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,59 @@
 import requests
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from django.views import View
 from rest_framework.generics import ListAPIView
 from rest_framework import serializers
 from .models import Show
+
+class ShowCreateView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'main_app/show_create.html')
+
+    def post(self, request, *args, **kwargs):
+        title = request.POST.get('title')
+        genre = request.POST.get('genre')
+        description = request.POST.get('description')
+        review = request.POST.get('review')
+
+        show = Show.objects.create(
+            title=title,
+            genre=genre,
+            description=description,
+            review=review,
+        )
+
+        return redirect('show_details', tv_id=show.tv_id)
+
+class ShowUpdateView(View):
+    def get(self, request, *args, **kwargs):
+        tv_id = kwargs.get('tv_id')
+        show = Show.objects.get(tv_id=tv_id)
+        return render(request, 'main_app/show_update.html', {'show': show})
+
+    def post(self, request, *args, **kwargs):
+        tv_id = kwargs.get('tv_id')
+        show = Show.objects.get(tv_id=tv_id)
+
+        show.title = request.POST.get('title')
+        show.genre = request.POST.get('genre')
+        show.description = request.POST.get('description')
+        show.review = request.POST.get('review')
+        show.save()
+
+        return redirect('show_details', tv_id=show.tv_id)
+
+class ShowDeleteView(View):
+    def get(self, request, *args, **kwargs):
+        tv_id = kwargs.get('tv_id')
+        show = Show.objects.get(tv_id=tv_id)
+        return render(request, 'main_app/show_delete.html', {'show': show})
+
+    def post(self, request, *args, **kwargs):
+        tv_id = kwargs.get('tv_id')
+        show = Show.objects.get(tv_id=tv_id)
+        show.delete()
+        return redirect('home')
 
 class ShowSerializer(serializers.ModelSerializer):
     class Meta:
@@ -34,3 +83,20 @@ def show_details(request, tv_id):
 
     context = {'data': data}
     return render(request, 'main_app/show_details.html', context)
+
+    # show details of a single tv show
+def show_details(request, tv_id):
+    api_key = '3d62d502968b0ef09de0fdbdfd9d6795'
+
+    # build URL to make the API request
+    url = f'https://api.themoviedb.org/3/tv/{tv_id}?api_key={api_key}&language=en-US'
+    response = requests.get(url)
+    data = response.json()
+
+    # pass the data to a template to render the TV show details
+    context = {'data': data}
+    return render(request, 'main_app/show_details.html', context)
+
+def show_form(request):
+    form = ShowForm()
+    return render(request, 'main_app/show_form.html', {'form': form})
